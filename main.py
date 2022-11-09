3100f2c61c273e479a5831886ab2b6a03763554c
bogal2006 <51925338+bogal2006@users.noreply.github.com>
2022-11-09 16:55:18 +0500

    test for github

--- main.py
+++ main.py
@@ -1,12 +1,11 @@
-from Flask import Flask
-
+from flask import Flask
 app=Flask(__name__)
 def calculate(a,b):
     c=a+b
     return c
 @app.route("/")
 def index():
-    return f"This is payment. Pay{calculate(10,20)}"
+    return f"This is payment. Pay{calculate(20,20)}"
 
 if __name__=="__main__":
     app.run(host="127.0.0.1",port=8080,debug=True)
---
+++ test1.py
@@ -0,0 +1,10 @@
+from main import calculate as cal
+def test(a,b):
+    c=cal(a,b)
+
+test_c=a+b
+assert c==test_c
+
+test(10,45)
+test(-5,89)
+test_c(89,90)
